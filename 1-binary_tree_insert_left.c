c735b46aeae2ec15b361677672e3314de07835e7
MosesOdary <mosesodary101@gmail.com>
2024-03-27 23:48:31 +0300

    Bug fix

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -3,13 +3,15 @@
 
 /**
  * binary_tree_insert_left -  insert node as the left-child of another node
+ *
  * @parent -pointer to node to insert left-child in
  * @value - value to store in the new node
+ *
  * Return: pointer to the created node, or NULL on failure or if parent is NULL
 */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *newTreeNode = malloc(sizeof(*newTreeNode));
+	binary_tree_t *newTreeNode = binary_tree_node(parent, value);
 
 	if (!parent)
 		return (NULL);
