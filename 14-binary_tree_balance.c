30896a205e5aca8803728825b0ca8d7d3ef3745a
MosesOdary <mosesodary101@gmail.com>
2024-03-28 01:05:42 +0300

    Task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,5 +8,38 @@
 */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	
+	if (tree)
+	{
+		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+	}
+
+	return (0);
 }
+
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	uint16_t i = 0;
+	uint16_t k = 0;
+
+	if (tree)
+	{
+		if (tree->left)
+			i = 1 + binary_tree_height(tree->left);
+		else
+			i = 1;
+		if (tree->right)
+			1 + binary_tree_height(tree->right);
+		else
+			k = 1;
+
+
+		if (i > k)
+			return (i);
+		else
+			return (k);
+	}
+
+	return (0);
+}
+
