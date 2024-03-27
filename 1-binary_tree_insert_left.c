ef77818ccb5469f72595b318eae18ff83828641d
MosesOdary <mosesodary101@gmail.com>
2024-03-27 23:55:11 +0300

    Fixed betty errors

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -4,8 +4,8 @@
 /**
  * binary_tree_insert_left -  insert node as the left-child of another node
  *
- * @parent -pointer to node to insert left-child in
- * @value - value to store in the new node
+ * @parent: pointer to node to insert left-child in
+ * @value: value to store in the new node
  *
  * Return: pointer to the created node, or NULL on failure or if parent is NULL
 */
@@ -15,7 +15,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	if (!parent)
 		return (NULL);
-	
+
 	if (!newTreeNode)
 		return (NULL);
 
