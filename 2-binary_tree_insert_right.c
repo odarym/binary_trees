dea3a23f04f97e6da36efc440b8207cfca17a715
= <=>
2024-03-28 02:42:54 +0300

    Minor fixes

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -5,7 +5,7 @@
  * binary_tree_insert_right - inserts a node as the right-child of another node
  * @parent: a pointer to node to insert the right-child in
  * @value:  value stored in the newTreeNode node
- * Return: a pointer to the created node, NULL on failure or if parent is NULL
+ * Return: a pointer to the created node/NULL on failure or if parent is NULL
 */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
