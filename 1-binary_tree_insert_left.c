a238b201d8e23a0e1b444c96503c59c26ccc3359
= <=>
2024-03-28 02:41:39 +0300

    Minor fixes

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -7,7 +7,7 @@
  * @parent: pointer to node to insert left-child in
  * @value: value to store in the new node
  *
- * Return: pointer to the created node, or NULL on failure or if parent is NULL
+ * Return: a pointer to created node, or NULL on failure or if parent is NULL
 */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
