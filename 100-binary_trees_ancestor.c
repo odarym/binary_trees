8343c4c9a8c749091bc9c8394b9eb6bb176a451e
MosesOdary <mosesodary101@gmail.com>
2024-03-28 02:22:30 +0300

    Task 100 fix

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -10,13 +10,65 @@
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 										const binary_tree_t *second)
 {
-	const binary_tree_t *firstAncestor = NULL;
-	const binary_tree_t *secondAncestor = NULL;
+	const binary_tree_t *firstAncestor = first;
+	const binary_tree_t *secondAncestor = second;
 
-	for (firstAncestor = first; firstAncestor; firstAncestor = firstAncestor->parent)
-		for (secondAncestor = second; secondAncestor; secondAncestor = secondAncestor->parent)
+	while (firstAncestor)
+	{
+		firstAncestor = firstAncestor->parent;
+		while (secondAncestor)
+		{
+			secondAncestor = secondAncestor->parent;
 			if (firstAncestor == secondAncestor)
 				return ((binary_tree_t *)firstAncestor);
+		}
+	}
 
 	return (NULL);
 }
+
+
+/**
+ * launch_test - Test ancestor function and print informations
+ *
+ * @n1: First node
+ * @n2: Second node
+ */
+void launch_test(binary_tree_t *n1, binary_tree_t *n2)
+{
+    binary_tree_t *ancestor;
+
+    ancestor = binary_trees_ancestor(n1, n2);
+    printf("Ancestor of [%d] & [%d]: ", n1->n, n2->n);
+    if (!ancestor)
+        printf("(nil)\n");
+    else
+        printf("%d\n", ancestor->n);
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    binary_tree_t *root;
+
+    root = binary_tree_node(NULL, 98);
+    root->left = binary_tree_node(root, 12);
+    root->right = binary_tree_node(root, 402);
+    root->left->right = binary_tree_node(root->left, 54);
+    root->right->right = binary_tree_node(root->right, 128);
+    root->left->left = binary_tree_node(root->left, 10);
+    root->right->left = binary_tree_node(root->right, 45);
+    root->right->right->left = binary_tree_node(root->right->right, 92);
+    root->right->right->right = binary_tree_node(root->right->right, 65);
+    binary_tree_print(root);
+
+    launch_test(root->left, root->right);
+    launch_test(root->right->left, root->right->right->right);
+    launch_test(root->right->right, root->right->right->right);
+    return (0);
+}
+
