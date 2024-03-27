2382834f12896eb148d2d78cdb75583985af0696
= <=>
2024-03-27 22:10:41 +0300

    Betty comments

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -2,7 +2,10 @@
 
 
 /**
- * 
+ * binary_tree_insert_left -  insert node as the left-child of another node
+ * @parent -pointer to node to insert left-child in
+ * @value - value to store in the new node
+ * Return: pointer to the created node, or NULL on failure or if parent is NULL
 */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -2,7 +2,9 @@
 
 
 /**
- * 
+ * binary_tree_is_a_leaf - function that checks if a node is a leaf
+ * @node - a pointer to the node to check
+ * Return: 1 if node is a leaf, 0 if otherwise, 0 
 */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
