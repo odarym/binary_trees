13b0348570262b6f51e3f4a740d53702c6ebcd25
= <=>
2024-03-28 02:44:44 +0300

    Betty documentation correction

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -2,8 +2,8 @@
 
 
 /**
- * binary_tree_is_leaf - function that checks if a node is a leaf
- * @node: a pointer to the node to check
+ * binary_tree_is_leaf - the function checks if a node is a leaf
+ * @node: pointer to the node to check
  * Return: 1 if node is a leaf, 0 if otherwise, 0 if node is NULL
 */
 int binary_tree_is_leaf(const binary_tree_t *node)
