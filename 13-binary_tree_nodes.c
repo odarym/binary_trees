b21a36796bf5a6324148e59bd035a57faf472aeb
MosesOdary <mosesodary101@gmail.com>
2024-03-28 00:51:01 +0300

    Task 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -8,5 +8,15 @@
 */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	
+	uint16_t nodes = 0;
+
+	if (tree)
+	{
+		if ((tree->left) || (tree->right))
+			nodes++;
+
+		nodes += binary_tree_nodes(tree->left);
+		nodes += binary_tree_nodes(tree->right);
+	}
+	return (nodes);
 }
