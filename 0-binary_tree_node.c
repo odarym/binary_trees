1a4919b794dc5a5289723e3bcb31aecaa750e42f
MosesOdary <mosesodary101@gmail.com>
2024-03-27 21:30:44 +0300

    Function to add a node to a tree

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -6,5 +6,17 @@
 */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
+	binary_tree_t *newTreeNode = malloc(sizeof(newTreeNode));
 
+	if (!newTreeNode)
+	{
+		return (NULL);
+	}
+
+	newTreeNode->n = value;
+	newTreeNode->parent = parent;
+	newTreeNode->left = NULL;
+	newTreeNode->right = NULL;
+
+	return (newTreeNode);
 }
