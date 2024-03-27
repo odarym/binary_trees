8e0403c4399188b9c32efd17e89ac714b2f9cd3f
MosesOdary <mosesodary101@gmail.com>
2024-03-28 01:54:05 +0300

    Task 18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -10,5 +10,11 @@
 */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	
+	if ((!node) || (!node->parent) || (!node->parent->parent))
+		return (NULL);
+
+	if (node->parent->parent->left == node->parent)
+		return (node->parent->parent->right);
+
+	return (node->parent->parent->left);
 }
