89778ded9c0c4053c44bde46ea90258e8fd82e63
= <=>
2024-03-28 04:38:35 +0300

    Task 112

--- 112-array_to_bst.c
+++ 112-array_to_bst.c
@@ -5,7 +5,7 @@
  * array_to_bst - function that builds a Binary Search Tree from an array
  * @array: pointer to the first element of the array to be converted
  * @size: number of element in the array
- * Return: pointer to the root node of the created BST, or NULL on failure
+ * Return: pointer to root node of the created BST, or NULL on failure
  * If a value of array is already present in tree, this value must be ignored
 */
 bst_t *array_to_bst(int *array, size_t size)
