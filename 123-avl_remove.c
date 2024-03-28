d851b6dcac886fbeb3b98aa48711456ae3e7525a
= <=>
2024-03-28 04:34:12 +0300

    Task 123

--- 123-avl_remove.c
+++ 123-avl_remove.c
@@ -4,7 +4,7 @@
  * avl_remove - function that removes a node from an AVL tree
  * @root: pointer to the root node of the tree for removing a node
  * @value: value to be removed in the tree
- * Return: pointer to new root node of tree after removing the desired value
+ * Return: pointer to new root node of tree after removing desired value
 */
 avl_t *avl_remove(avl_t *root, int value)
 {
