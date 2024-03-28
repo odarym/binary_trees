77808bf3dfa27ce5eea4e5c926d01b3830f26bf1
= <=>
2024-03-28 03:42:08 +0300

    Bug fixes

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -1,6 +1,4 @@
 #include "binary_trees.h"
-
-
 /**
  * bst_remove - function that removes a node from a Binary Search Tree
  * @root: pointer to the root node of the tree where you will remove a node
@@ -9,5 +7,47 @@
 */
 bst_t *bst_remove(bst_t *root, int value)
 {
+	bst_t *tmp = NULL;
 
+	if (!root)
+		return (NULL);
+
+	if (value < root->n)
+		root->left = bst_remove(root->left, value);
+	else if (value > root->n)
+		root->right = bst_remove(root->right, value);
+	else
+	{
+		if (!root->left)
+		{
+			tmp = root->right;
+			free(root);
+			return (tmp);
+		}
+		else if (!root->right)
+		{
+			tmp = root->left;
+			free(root);
+			return (tmp);
+		}
+		tmp = bst_min_val(root->right);
+		root->n = tmp->n;
+		root->right = bst_remove(root->right, tmp->n);
+	}
+	return (root);
+}
+
+/**
+ * bst_min_val - finds smallest node from Binary Search Tree
+ * @root: a pointer to root node of tree
+ * Return: a pointer to smallest node
+ */
+bst_t *bst_min_val(bst_t *root)
+{
+	bst_t *min = root;
+
+	while (min->left)
+		min = min->left;
+
+	return (min);
 }
