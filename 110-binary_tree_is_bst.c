82d89d78d135e7e05d6048112c6e4d1ac04d0318
MosesOdary <mosesodary101@gmail.com>
2024-03-28 03:14:37 +0300

    Task 24

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,4 +1,20 @@
+#include <limits.h>
+
 #include "binary_trees.h"
+
+
+int isBST(const binary_tree_t *tree, int min, int max)
+{
+	if (tree == NULL)
+		return (1);
+
+	if (tree->n < min || tree->n > max)
+		return (0);
+
+	return (
+		isBST(tree->left, min, tree->n - 1) &&
+		isBST(tree->right, tree->n + 1, max));
+}
 
 
 /**
@@ -8,5 +24,8 @@
 */
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
+	if (tree == NULL)
+		return (0);
 
+	return (isBST(tree, INT_MIN, INT_MAX));
 }
