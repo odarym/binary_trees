1e312d650e31a7b449487ffabd31a8be92fb8fb9
= <=>
2024-03-28 04:29:41 +0300

    Task 134

--- 134-heap_to_sorted_array.c
+++ 134-heap_to_sorted_array.c
@@ -2,8 +2,8 @@
 
 /**
  * heap_to_sorted_array - converts Binary Max Heap to sorted array of integers
- * @heap: pointer to the root node of the heap to convert
- * @size: address to store the size of the array
+ * @heap: pointer to the root node of heap to convert
+ * @size: address to store the; size of the array
  * Return: array returned must be sorted in descending order
 */
 int *heap_to_sorted_array(heap_t *heap, size_t *size)
